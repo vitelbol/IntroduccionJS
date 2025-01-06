cb570b793ef2098e8f7f103413c1f4318fbb98c0
jose villegas <josevillegas13600@gmail.com>
2025-01-05 22:19:21 -0400

    numeros

---
+++ js/js05.js
@@ -0,0 +1,15 @@
+//numeros
+const nombre ="jose";
+const numero1 = 100;
+const numero2 = 200;
+const numero3 =200.20;
+const numero4 =7;
+const numero5 =6;
+
+console.log(nombre);
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1);
+console.log(numero4 % numero5);

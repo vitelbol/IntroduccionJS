150c6089579f481cf2cbb287187e8b25eb5bb69f
jose villegas <josevillegas13600@gmail.com>
2025-01-05 22:02:06 -0400

    string

---
+++ js/js04.js
@@ -0,0 +1,9 @@
+//string o cadenas de texto
+const producto = "Monitor de 20 Pulgadas";
+const producto2 = String('Monitor 30 pulgadas');
+const producto3 = new String('Monitor 50 Pulgadas')
+console.log(producto)
+//tipo de dato
+console.log(typeof producto);
+console.log(typeof producto2);
+console.log(typeof producto3)

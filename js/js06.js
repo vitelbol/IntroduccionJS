aa5142f53c8cdfbfd8b6c3f9e02868c97474783a
jose villegas <josevillegas13600@gmail.com>
2025-01-05 22:29:07 -0400

    funcionmath

---
+++ js/js06.js
@@ -0,0 +1,14 @@
+//objeto math
+let resultado;
+resultado= Math.PI;
+//redondeo
+resultado = Math.round(2.7);
+resultado = Math.ceil(2.2); // ceil siempre redondea hacia un numero mayor
+resultado = Math.floor(2.9); //floor redondea al numero menor
+resultado =Math.sqrt(144); //raiz cuadrada
+resultado = Math.abs(-200); //convierte negativo a positivo
+resultado = Math.min(3,5,1,8,2,10); // da el menor valor
+resultado = Math.max(3,5,1,8,2,10); // da el mayor valor
+resultado = Math.random(); //numeros alatorio
+resultado = Math.floor(Math.random()*30);
+console.log(resultado);

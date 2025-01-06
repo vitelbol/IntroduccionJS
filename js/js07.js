64971d0077746362ccad529138d52a2e1a1e90ab
jose villegas <josevillegas13600@gmail.com>
2025-01-05 22:39:08 -0400

    ordenoperaciones

---
+++ js/js07.js
@@ -0,0 +1,16 @@
+//orden de las operaciones
+let resultado;
+
+resultado = (20+30)*2;
+resultado = (100+200+300)*.2;
+
+//console.log(resultado);
+//incrementos
+let puntaje = 10;
+//incremento en 1 ++
+//puntaje++;//10
+//++puntaje; //12
+//decremento --
+//puntaje--;
+puntaje +=10;
+console.log(puntaje);

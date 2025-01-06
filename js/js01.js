ecb472a55939eba46068d9053148006994f73558
jose villegas <josevillegas13600@gmail.com>
2025-01-05 21:53:46 -0400

    variables

---
+++ js/js01.js
@@ -0,0 +1,18 @@
+//Variables
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+var disponible //iniciamos la variable sin valor
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+//estilos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto ='Monitor HD'; //Pascal case
+var nombreproducto = 'MonitorHD'
+
+console.log(PRODUCTO);
---
+++ js/js03.js
@@ -0,0 +1,20 @@
+//Variables
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+const disponible =true;//iniciamos la variable sin valor
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+const disponible_ = true;
+
+//estilos para las variables
+const nombre_producto = 'Monitor HD'; //underscore
+const nombreProducto = 'Monitor HD'; //Camelcase
+const NombreProducto ='Monitor HD'; //Pascal case
+const nombreproducto = 'MonitorHD'
+
+console.log(PRODUCTO);

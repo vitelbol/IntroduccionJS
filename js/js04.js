6615a77cc2b720d85573136c8f76c34eee86a8f9
jose villegas <josevillegas13600@gmail.com>
2025-01-05 22:11:33 -0400

    metodosstring

--- js/js04.js
+++ js/js04.js
@@ -1,9 +1,19 @@
 //string o cadenas de texto
-const producto = "Monitor de 20 Pulgadas";
-const producto2 = String('Monitor 30 pulgadas');
-const producto3 = new String('Monitor 50 Pulgadas')
-console.log(producto)
+const tweet = 'Aprendiendo javascript con el curso desarrollo web';
+const producto2 = 'Monitor HD';
+//const producto2 = String('Monitor 30 pulgadas');
+//const producto3 = new String('Monitor 50 Pulgadas')
+console.log(tweet.length);
+console.log(producto2);
 //tipo de dato
-console.log(typeof producto);
-console.log(typeof producto2);
-console.log(typeof producto3)
+//console.log(typeof producto);
+//console.log(typeof producto2);
+//console.log(typeof producto3)
+
+//indexof dice que posicion se encuentra la palabra o letra
+console.log(tweet.indexOf('javascript'));
+console.log(producto2.indexOf('Tablet'));
+
+//includes retorna true o false
+console.log(tweet.includes('javascript'));
+console.log(producto2.includes('Tablet'));

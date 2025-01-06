e0179ef85018ad5cc8781d05d6fbab4433283f5d
jose villegas <josevillegas13600@gmail.com>
2025-01-05 22:47:31 -0400

    templatestring

---
+++ js/js08.js
@@ -0,0 +1,7 @@
+const nombre ='Jose';
+const email = 'correo@correo.com'
+//concatenacion
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+//template strings - string literals
+//usar comilla invertida `` alt+ llave cerrada '}'
+console.log(`Nombre Cliente: ${nombre} Email: ${email}` );

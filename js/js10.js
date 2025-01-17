06673d248122d40d7c0f006a71292a01ffe520a9
jose villegas <josevillegas13600@gmail.com>
2025-01-17 10:34:47 -0400

    objetos

---
+++ js/js10.js
@@ -0,0 +1,10 @@
+//objetos
+const nombreProducto = "Monitor 20 pulgadas"
+const precio = 300;
+const disponible = true;
+
+const producto ={
+    nombreProducto : "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true
+}

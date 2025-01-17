028e61f30d167ff3bb11ecfbb4fece93e37e6ac5
jose villegas <josevillegas13600@gmail.com>
2025-01-17 10:42:23 -0400

    modificarobjetos

--- js/js10.js
+++ js/js10.js
@@ -1,10 +1,22 @@
 //objetos
+/*
 const nombreProducto = "Monitor 20 pulgadas"
 const precio = 300;
 const disponible = true;
-
+*/
 const producto ={
     nombreProducto : "Monitor 20 pulgadas",
     precio: 300,
     disponible: true
 }
+/*
+console.log(producto.precio);
+console.log(producto.nombreProducto);
+console.log(producto.disponible);
+console.log(producto["precio"]);*/
+
+//agregarpropiedades
+producto.imagen ='imagen.jpg';
+//eliminar propiedades
+delete producto.disponible;
+console.log(producto);

3d5133d5f9616b387e35986358af7d4330da410f
jose villegas <josevillegas13600@gmail.com>
2025-05-21 21:07:56 -0400

    pooherencia

--- js/js28.js
+++ js/js28.js
@@ -11,10 +11,36 @@ class Producto
     {
         return `El producto ${this.nombre} tiene un precio de: $ ${this.precio}`;
     }
+    obtenerPrecio()
+    {
+        console.log(this.precio);
+    }
 }
 
 const producto2 = new Producto('Monitor curvo de 49 "',800);
 const producto3 = new Producto('Laptop',300);
 
+//Herencia
+
+
+class Libro extends Producto
+{
+    constructor(nombre, precio, isbn)
+    {
+        super(nombre,precio);
+        this.isbn = isbn;
+    }
+    formatearProducto()
+    {
+        return `${super.formatearProducto()} y su ISBN es : ${this.isbn}`;
+    }
+    obtenerPrecio(){
+        super.obtenerPrecio();
+        console.log(' Y si hay en existencia');
+    }
+}
+const libro = new Libro('Javascript la revolucion',120,'236565622322');
+console.log(libro.formatearProducto());
+console.log(libro.obtenerPrecio());
 console.log(producto2);
 console.log(producto3);

a61412fef651fde4eabe1a7488fb88683a84fe05
jose villegas <josevillegas13600@gmail.com>
2025-05-21 21:57:16 -0400

    dosconsultasasyncawait

--- js/js32.js
+++ js/js32.js
@@ -9,12 +9,27 @@ function descargarNuevosClientes()
     });
 }
 
+function descargarUltimosClientes()
+{
+    return new Promise (resolve => {
+        console.log('Descargando pedidos... espere...');
+        setTimeout(() => {
+            resolve('Los pedidos fueron descargados');
+        }, 3000);
+    });
+}
+
 async function app()
 {
     try {
-        const resultado = await descargarNuevosClientes();
-        console.log('este codigo si se bloquea');
-        console.log(resultado);
+        //const clientes = await descargarNuevosClientes();
+        //const pedidos = await descargarUltimosClientes();
+        //console.log('este codigo si se bloquea');
+        //console.log(clientes);
+        //console.log(pedidos);
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosClientes()]);
+        console.log(resultado[0]);
+        console.log(resultado[1]);
     } catch (error) {
         console.log(error);
     }
